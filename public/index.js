00a3729ee0ef5edf998b84a4b67aa26df0698335
young kui <yyk7king@gmail.com>
2019-06-01 01:59:25 +0900

    top 높이값 왜 동적으로 생기기전에 가져오지?

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ window.onload = function(){
     document.getElementById("get").addEventListener("click", btn_get);
     document.getElementById("clear").addEventListener("click", btn_clear);
 }
+
 let count = 0,
     timer;
 
@@ -23,29 +24,22 @@ function data_request(count){
 function data_append(arr, count) {
     let arr_slice = arr.slice(4*(count - 1), 4*count);
     
-    arr_slice.forEach(function(item, index){
+    arr_slice.forEach(function(item){
         let dom_img = document.createElement("img"),
             dom_li = document.createElement("li"),
-            arr_top = [];
-
+            dom_li_all = document.querySelectorAll('.list li'),
+            dom_li_count = 'list'+dom_li_all.length;
+        
+        dom_li.id = dom_li_count;
         dom_img.src = item;
         dom_li.appendChild(dom_img);
         document.getElementById("list").appendChild(dom_li);
 
-        arr_top.push(dom_li.clientHeight);
-        console.log(arr_top);
+        console.log(document.getElementById(dom_li_count).clientHeight);       
     });
- 
-    // let out = "",
-    //     i;
-    // for(i = 0; i < arr.length; i++) {
-    //     out += '<li><img src="' + arr[i] + '"></li>';
-    // }
-    // document.getElementById("list").appendChild('<li>테스트1</li>');
-    // document.getElementById("list").innerHTML = out;
 }
 
-function btn_get(e){
+function btn_get(){
     if (timer) {
         clearTimeout(timer);
     }
@@ -53,13 +47,13 @@ function btn_get(e){
     timer = setTimeout(function(){
         count++;
         data_request(count);
-    },200);
+    },100);
     
 }
 
 function btn_clear(){
     let ele_ul = document.querySelectorAll(".list li");
-    
+    count = 0;
     ele_ul.forEach(function(item){
         item.parentNode.removeChild(item);
     });
